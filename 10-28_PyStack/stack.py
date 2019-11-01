79c64b4b7f911d578bd73532b3fc5b7614946725
Nathan Koike <nathankoike@nathans-air.hamilton.edu>
2019-11-01 19:00:47 -0400

    finished(?) push method for node-based stack

--- 10-28_PyStack/stack.py
+++ 10-28_PyStack/stack.py
@@ -7,7 +7,7 @@ Desc: create a module to implement a stack in python using both lists and nodes
 # a node for a singly linked list
 class Node:
     # allow a node to be made without any data
-    def __init__(self, value=None, next=None):
+    def __init__(self, val=None, next=None):
         self._value = value # the data in the node
         self._next = next # the next node
 
@@ -26,6 +26,19 @@ class Node:
     # get the node immediately following this node
     def get_next(self):
         return self._next
+
+class Stack:
+    # allow the stack to be initialized with or without a top value
+    def __init__(self, top=None):
+        self._top = top
+
+    def push(self, value):
+        # create a new node containing the desired value immidiately before the
+        # top of this stack
+        new_node = Node(val=value, next=self._top)
+
+        # set the top of the stack to reflect this new addition
+        self._top = new_node
 
 # a list-based stack implementation
 class L_Stack:
