d7eef7b8bb0d2e5dfa0e6c5e671b84dfa2a2f110
Nathan Koike <nathankoike@nathans-air.hamilton.edu>
2019-11-01 18:56:30 -0400

    finished(?) node class

--- 10-28_PyStack/stack.py
+++ 10-28_PyStack/stack.py
@@ -4,6 +4,30 @@ Date: 1 November, 2019
 Desc: create a module to implement a stack in python using both lists and nodes
 """
 
+# a node for a singly linked list
+class Node:
+    # allow a node to be made without any data
+    def __init__(self, value=None, next=None):
+        self._value = value # the data in the node
+        self._next = next # the next node
+
+    # set the value of this node
+    def set_val(self, value):
+        self._value = value
+
+    # get the value from this node
+    def get_val(self):
+        return self._value
+
+    # set the node immediately following this node
+    def set_next(self, next):
+        self._next = next
+
+    # get the node immediately following this node
+    def get_next(self):
+        return self._next
+
+# a list-based stack implementation
 class L_Stack:
     # the constructor for an empty stack
     def __init__(self):
