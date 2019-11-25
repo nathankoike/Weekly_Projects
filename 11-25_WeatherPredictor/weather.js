aed41929ba690cd3683a89cff81ddf7ed2825b80
Nathan Koike <nathankoike@nathans-air.hamilton.edu>
2019-11-25 02:56:51 -0500

    added a readme and started the next project

---
+++ 11-25_WeatherPredictor/weather.js
@@ -0,0 +1,13 @@
+/*
+predict future weather patterns in a city based on past data from Dark Sky's
+database
+API hook in call
+*/
+
+// get data from Dark Sky using the key supplied on the given date
+function getData(key, date) {
+  return;
+}
+
+// get the key from the call to the program
+var key = process.argv[0];
