84385274bb21e2954e66f99753d6769c2707d1ae
Nathan Koike <nathankoike@nathans-air.hamilton.edu>
2019-11-26 16:55:42 -0500

    added more functional decomposition to weather project

--- 11-25_WeatherPredictor/weather.js
+++ 11-25_WeatherPredictor/weather.js
@@ -1,13 +1,45 @@
 /*
-predict future weather patterns in a city based on past data from Dark Sky's
-database
-API hook in call
+Auth: Nate Koike
+Date: 25 November 2019
+Desc: predict future weather patterns in a city based on past data
+
+Note: this is currently just a terminal application, but i might build an HTML
+      interface later
+      to run this program use "node weather [weatherKey] [geocodeKey] [city]"
 */
 
+const request = require("request");
+
+// the base url that we will modify for weather requests
+const WEATHER = "https://api.darksky.net/forecast/";
+
+// the base url that we will modify for location requests
+const LATLNG = ""; // i am still deciding on
+
 // get data from Dark Sky using the key supplied on the given date
-function getData(key, date) {
+function getData(weatherURL) {
+  request({ url: weatherURL, json: true }, function(err, res) {
+    temp = res.body.currently.temperature;
+  });
+}
+
+// get the latitude and longitude of a city
+function getCoords(city) {
   return;
 }
 
-// get the key from the call to the program
-var key = process.argv[0];
+// the main function that will handle all the processing
+function main() {
+  // get the key from the comamnd line call to the program
+  var key = process.argv[2];
+
+  // get the name of the city
+  var city = process.argv[3];
+
+  // get the coordinates of the city
+  var coords = getCoords(city);
+}
+
+main();
+
+console.log(URL);
